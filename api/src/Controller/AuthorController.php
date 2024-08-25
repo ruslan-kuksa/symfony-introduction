2fa27ff0d0cfb2ba50d84ea34c583a18dc0fe4c3
Ruslan Kuksa <ipz221@gmail.com>
2024-08-26 01:04:44 +0300

    added mailer

--- api/src/Controller/AuthorController.php
+++ api/src/Controller/AuthorController.php
@@ -17,7 +17,6 @@ class AuthorController extends AbstractController
     public function __construct(
         protected SerializerInterface $serializer,
         protected EntityManagerInterface $entityManager,
-        protected UserPasswordHasherInterface $userPasswordHasher,
     )
     {}
     #[Route('/api/author-create', name: 'app_author', methods: ['POST'])]
@@ -29,16 +28,49 @@ class AuthorController extends AbstractController
         $this->entityManager->persist($author);
         $this->entityManager->flush();
 
-        $jsonData = $this->serializer->serialize($author, 'json', ['groups' => 'user:read']);
+        $jsonData = $this->serializer->serialize($author, 'json');
 
         return new JsonResponse($jsonData, Response::HTTP_CREATED, [], true);
     }
 
-    #[Route('/api/author-list/{id}', name: 'app_author_list', methods: ['GET'])]
+    #[Route('/api/author-list', name: 'app_author_list', methods: ['GET'])]
     public function list_authors(Request $request, string $id): JsonResponse
     {
-        $data = $this->entityManager->getRepository(Author::class)->findOneBy($id);
+        $data = $this->entityManager->getRepository(Author::class)->findAll();
 
-        $jsonData = $this->serializer->deserialize($request->getContent(), Author::class, 'json');
+        $jsonData = $this->serializer->serialize($data, 'json');
+
+        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+
+    }
+
+    #[Route('/api/author/{id}', name: 'app_author', methods: ['GET'])]
+    public function get_author(Request $request, string $id): JsonResponse
+    {
+        $data = $this->entityManager->getRepository(Author::class)->findOneBy(['id' => $id]);
+
+        $jsonData = $this->serializer->serialize($data, 'json');
+
+        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/author/{id}/edit', name: 'app_author_edit', methods: ['PUT'])]
+    public function edit_author(Request $request, string $id): JsonResponse
+    {
+        $author = $this->entityManager->getRepository(Author::class)->findOneBy(['id' => $id]);
+        $data = $this->serializer->deserialize($request->getContent(), Author::class, 'json', ['object_to_populate' => $author]);
+        $this->entityManager->persist($author);
+        $this->entityManager->flush();
+        $jsonData = $this->serializer->serialize($data, 'json');
+        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/author/{id}/delete', name: 'app_author_delete', methods: ['DELETE'])]
+    public function delete_author(Request $request, string $id): JsonResponse
+    {
+        $author = $this->entityManager->getRepository(Author::class)->findOneBy(['id' => $id]);
+        $this->entityManager->remove($author);
+        $this->entityManager->flush();
+        return new JsonResponse("Author Deleted", Response::HTTP_OK, [], true);
     }
 }
--- api/src/Controller/BookController.php
+++ api/src/Controller/BookController.php
@@ -2,17 +2,69 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class BookController extends AbstractController
 {
-    #[Route('/book', name: 'app_book')]
-    public function index(): Response
+    public function __construct(
+        protected SerializerInterface $serializer,
+        protected EntityManagerInterface $entityManager,
+    )
+    {}
+    #[Route('/api/book-create', name: 'app_author', methods: ['POST'])]
+    public function bookCreate(Request $request): Response
     {
-        return $this->render('book/index.html.twig', [
-            'controller_name' => 'BookController',
-        ]);
+        $book = $this->serializer->deserialize($request->getContent(), Book::class, 'json');
+
+        $this->entityManager->persist($book);
+        $this->entityManager->flush();
+
+        $jsonData = $this->serializer->serialize($book, 'json');
+
+        return new JsonResponse($jsonData, Response::HTTP_CREATED, [], true);
+    }
+
+    #[Route('/api/book-list', name: 'app_author', methods: ['GET'])]
+    public function bookList(Request $request): Response
+    {
+        $books = $this->entityManager->getRepository(Book::class)->findAll();
+        $jsonData = $this->serializer->serialize($books, 'json');
+        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/book/{id}', name: 'app_author', methods: ['GET'])]
+    public function book(Request $request, string $id): Response
+    {
+        $book = $this->entityManager->getRepository(Book::class)->findOneBy(['id' => $id]);
+        $jsonData = $this->serializer->serialize($book, 'json');
+        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/book/{id}', name: 'app_author', methods: ['DELETE'])]
+    public function bookDelete(Request $request, string $id): Response
+    {
+        $book = $this->entityManager->getRepository(Book::class)->findOneBy(['id' => $id]);
+        $this->entityManager->remove($book);
+        $this->entityManager->flush();
+        $jsonData = $this->serializer->serialize($book, 'json');
+        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/book/{id}', name: 'app_author', methods: ['PUT'])]
+    public function bookUpdate(Request $request, string $id): Response
+    {
+        $book = $this->entityManager->getRepository(Book::class)->findOneBy(['id' => $id]);
+        $data = $this->serializer->deserialize($request->getContent(), Book::class, 'json', ['object_to_populate' => $book]);
+        $this->entityManager->persist($book);
+        $this->entityManager->flush();
+        $jsonData = $this->serializer->serialize($data, 'json');
+        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
     }
 }
